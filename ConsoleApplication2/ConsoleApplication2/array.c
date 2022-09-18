34db1b14d7d5796f913d2f8291d491b47470527d
z7qyrr6prr@privaterelay.appleid.com <z7qyrr6prr@privaterelay.appleid.com>
2022-09-18 22:18:01 +0800

    55

---
+++ ConsoleApplication2/ConsoleApplication2/array.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	int d = 1;
+//	//.....
+//
+//	// so ,use the array to solve the complex problem
+//	int arr[10]= {1,2,3,4,5,6,7,8,9,10};
+//	/*Create and initialize of the array
+//	使用下标访问*/
+//	int i = 0;
+//	while(i<10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	char ch[5] = { 'a','b','c' }; //incomplete initialization , the rest will be filled with 0.
+//
+//
+//	return 0;
+//
+//}
